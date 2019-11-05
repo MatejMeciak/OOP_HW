f5d3c2da7b9bf93ae84b8f04c8e5ac3aa7f3e176
Matej Meciak <matej.meciak123@gmail.com>
2019-11-05 07:44:41 +0100

    Add project files.

---
+++ Dungeon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP___HW
+{
+    class Dungeon
+    {
+        private readonly List<Mob> mobs;
+        public Dungeon()
+        {
+            mobs = new List<Mob>
+            {
+                new Mob("Big Foot", 100),
+                new Mob("Alien Brain", 150),
+                new Mob("Mad Toast", 300),
+                new Mob("Dudy", 500),
+                new Mob("ULTRABIGSUPERSKELLY", 1000),
+            };
+                
+        }
+        public void Update()
+        {
+            foreach(var mob in mobs)
+            {
+                mob.Refresh();
+            }
+        }
+    }
+                
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP___HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dungeon = new Dungeon();
+            var weapon = new Weapon(10);
+            while (true)
+            {
+                dungeon.Update();
+                weapon.DoDamage(10);
+
+            }
+        }
+    }
+}
